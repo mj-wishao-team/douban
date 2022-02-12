9f7b20efd0bef942262a72fb12fe249a6c915bdb
9WiSHao <825763548@qq.com>
2022-02-12 16:29:17 +0800

    feat: 账号设置页面样式部分

--- static/accounts/accountSetting/accountSetting.js
+++ static/accounts/accountSetting/accountSetting.js
@@ -1,7 +1,58 @@
-let accessToken = localStorage.getItem('access_token');
-let refreshToken = localStorage.getItem('refresh_token');
+// 头部导航栏部分
+const noneAll = document.querySelectorAll('.nav-left a');
+const movie = document.querySelector('.movie');
+const settings = document.querySelector('#settings');
+const myPage = document.querySelector('#my-page');
+const none1 = document.querySelector('#none1');
+const none2 = document.querySelector('#none2');
+const accountSetting = document.querySelector('#account-setting');
+const logout = document.querySelector('#logout');
+document.querySelector('.account')['onclick'] = () => {
+    if (settings.style.display == 'none') {
+        settings.style.display = 'block';
+    } else {
+        settings.style.display = 'none';
+    }
+}
+for (let i = 0; i < noneAll.length; i++) {
+    noneAll[i].onclick = () => {
+        alert('没有这个功能😭')
+    }
+}
+movie.onclick = () => {
+    window.location.href = "../../../index/main/main.html"
+}
+myPage.onclick = () => {
 
+}
+none1.onclick = () => {
+    alert('没有这个功能😭')
+}
+none2.onclick = () => {
+    alert('没有这个功能😭')
+}
+accountSetting.onclick = () => {
+    window.location.href = "../accountSetting.html"
+}
+logout.onclick = () => {
+    localStorage.clear();
+    window.location.href = "../../../index/main/main.html"
+}
 
-document.querySelector('.button')['onclick'] = () => {
-
+// 点击切换功能
+const otherSetup = document.querySelector('#other-setup');
+const basicSetup = document.querySelector('#basic-setup');
+const basic = document.querySelector('.basic');
+const others = document.querySelector('.others');
+otherSetup.onclick = () => {
+    basicSetup.className = 'unselected';
+    otherSetup.className = 'selected';
+    basic.style.display = 'none';
+    others.style.display = 'block';
+}
+basicSetup.onclick = () => {
+    basicSetup.className = 'selected';
+    otherSetup.className = 'unselected';
+    basic.style.display = 'flex';
+    others.style.display = 'none';
 }
